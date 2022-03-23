fa0226e1e5b49afb5675dff4ff004dd143d730d1
TejShind <tej.gholap25@gmail.com>
2022-03-23 13:31:54 +0530

    [Tejaswini]ADDED:Feature for Modified PinCode if space is available in between numbers that PinCode also valid

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 2) Create Regex pattern to check functionality of validate PinCode
 3) Add Alphabet at start of PinCode.
 4) Add Alphabet at end of PinCode.
+5) Creating space in between PinCode number.
     
--- src/com/blz/regex/PinCodeRegex.java
+++ src/com/blz/regex/PinCodeRegex.java
@@ -5,15 +5,16 @@ import java.util.regex.Pattern;
 
 public class PinCodeRegex {
     public static void main(String[] args) {
-        String pinCode = "400088X";
-        String regex = "^[1-9]{1}[0-9]{5}$";
+        String pinCode = "400 088";
+        //String regex = "^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$";//for space
+        String regex = "^[1-9]{1}[0-9]{2}\\s?[0-9]{3}$";//for space
 
         Pattern pattern = Pattern.compile(regex);//Java class
         Matcher matcher = pattern.matcher(pinCode);// Java class
         if (matcher.matches())
             System.out.println("PinCode is Valid");
         else
-            System.out.println("PinCode is Invalid because there is alphabet at End");
+            System.out.println("PinCode is Invalid");
     }
 }
 
