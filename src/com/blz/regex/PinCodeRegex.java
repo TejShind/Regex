97b052fd88240bdd7e2423d2bce47941a055b068
TejShind <tej.gholap25@gmail.com>
2022-03-23 13:05:15 +0530

    [Tejaswini]ADDED:Feature for Create Regex pattern to check functionality of validate PinCode

--- src/com/blz/regex/PinCodeRegex.java
+++ src/com/blz/regex/PinCodeRegex.java
@@ -1,8 +1,19 @@
 package com.blz.regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PinCodeRegex {
     public static void main(String[] args) {
-        System.out.println("Welcome message");
+        String pinCode = "400088";
+        String regex = "^[0-9]{6}$";
+
+        Pattern pattern = Pattern.compile(regex);//Java class
+        Matcher matcher = pattern.matcher(pinCode);// Java class
+        if (matcher.matches())
+            System.out.println("PinCode is Valid");
+        else
+            System.out.println("PinCode is Invalid ");
     }
 }
 
