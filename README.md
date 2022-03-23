484fe64b5ce142efca86542a1b666d8c159fd9b1
TejShind <tej.gholap25@gmail.com>
2022-03-23 13:15:16 +0530

    [Tejaswini]ADDED:Feature for Modified PinCode by adding alphabet at Start

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # REGEX
 1)Welcome message.
+2) Create Regex pattern to check functionality of validate PinCode
+3) Add Alphabet at start of PinCode.
+4) Add Alphabet at end of PinCode.
+    
--- src/com/blz/regex/PinCodeRegex.java
+++ src/com/blz/regex/PinCodeRegex.java
@@ -5,15 +5,15 @@ import java.util.regex.Pattern;
 
 public class PinCodeRegex {
     public static void main(String[] args) {
-        String pinCode = "400088";
-        String regex = "^[0-9]{6}$";
+        String pinCode = "M400088";
+        String regex = "^[1-9]{1}[0-9]{5}$";
 
         Pattern pattern = Pattern.compile(regex);//Java class
         Matcher matcher = pattern.matcher(pinCode);// Java class
         if (matcher.matches())
             System.out.println("PinCode is Valid");
         else
-            System.out.println("PinCode is Invalid ");
+            System.out.println("PinCode is Invalid because there is alphabet at Start");
     }
 }
 
